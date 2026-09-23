6de2b03dfc9e9f81db96d5f6c3aaf2d79a341045
mansoor shaik <mansoorshaik364@gmail.com>
2026-09-23 21:34:55 +0530

    array

---
+++ 06-Array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+fruits=("Apple" "Kiwi" "Graphes" "lichie")
+
+echo "first fruit is : ${fruits[0]}"
+echo "second fruit is : ${fruits[1]}"
+echo "third fruit is : ${fruits[2]}"
+echo "All fruits in Array are :${fruits[@]} "
