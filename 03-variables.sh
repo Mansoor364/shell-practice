99efead92969e17eaadc60e1dbb25de67fb28b02
mansoor shaik <mansoorshaik364@gmail.com>
2026-09-23 20:30:27 +0530

    updating variables in file

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#variables
+Person1=Ramesh
+Person2=suresh
+
+echo "$Person1:: Hi, $Person2 How are you??"
+echo "$Person2:: Hello! ${Person1} Iam great"
+echo "$Person1 - How are you doing? $Person2"
+echo "$Person2 - Iam learning shell ${Person1}"
