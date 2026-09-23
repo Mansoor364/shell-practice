1e7e0013bb3731d66a4541c83a4b64cb27eb27c9
mansoor shaik <mansoorshaik364@gmail.com>
2026-09-23 20:07:55 +0530

    conversations

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#conversation between 2 people
+
+echo "Ramesh :: Hi Suresh!! How are you??"
+echo "Suresh :: Iam good Ramesh."
+echo "Ramesh : How are you doing Suresh"
+echo "Suresh : I'm practicing shell-scripting Ramesh"
