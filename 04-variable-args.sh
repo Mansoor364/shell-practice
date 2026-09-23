858fd610f8f3c3da965f61d59b2efc36b76a9aa7
mansoor shaik <mansoorshaik364@gmail.com>
2026-09-23 20:40:54 +0530

    passing arguments to variables

---
+++ 04-variable-args.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1:: Hi $Person2 , How are you??"
+echo "$Person2: I'm doing great $Person1.."
+echo "$Person1 - where are you $Person2"
+echo "$Person2 - I'm in Hyderabad, $Person1"
